819b512b98b347f22639cb47622a7c2ef6717cd2
kristina-pachi <kristina.pachi.04@bk.ru>
2026-04-17 21:21:04 +0300

    Задание 3.1: Иерархия транспортных средств

--- part3/part3_1/model/ElectricCar.java
+++ part3/part3_1/model/ElectricCar.java
@@ -24,7 +24,7 @@ public class ElectricCar extends Car implements Electric {
     public double getRangeKm() {
         // TODO: запас хода = maxRangeKm * batteryLevel.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return 0.0;
+        return maxRangeKm * batteryLevel;
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
@@ -32,7 +32,10 @@ public class ElectricCar extends Car implements Electric {
     public void charge(double hours) {
         // TODO: +20% в час, максимум 100%.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-
+        batteryLevel += 0.2 * hours;
+        if (batteryLevel > 1.0) {
+            batteryLevel = 1.0;
+        }
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
