72c9a3287a5d912f3c221d77ee9c0f387c07216d
kristina-pachi <kristina.pachi.04@bk.ru>
2026-04-17 20:57:29 +0300

    Задание 2.2: Числа Фибоначчи

--- part2/part2_2/Fibonacci.java
+++ part2/part2_2/Fibonacci.java
@@ -6,21 +6,57 @@ public class Fibonacci {
         // TODO: реализуйте while-вариант.
         // Подсказка: базовые случаи n=0 и n=1 обработайте отдельно.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return -1;
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            long a = 0;
+            long b = 1;
+            int i = 2;
+
+            while (i <= n) {
+                long next = a + b;
+                a = b;
+                b = next;
+                i++;
+            }
+
+            return b;
+
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
     public static long fibFor(int n) {
         // TODO: реализуйте for-вариант.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return -1;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        long a = 0;
+        long b = 1;
+
+        for (int i = 2; i <= n; i++) {
+            long next = a + b;
+            a = b;
+            b = next;
+        }
+
+        return b;
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
     public static long firstGreaterThan(long threshold) {
         // TODO: найдите первое число Фибоначчи > threshold.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return -1;
+        long a = 0;
+        long b = 1;
+
+        while (b <= threshold) {
+            long next = a + b;
+            a = b;
+            b = next;
+        }
+
+        return b;
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
